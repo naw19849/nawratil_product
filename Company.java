e764be394e3b1e5cbc622944c559fb9270169078
= <=>
2020-04-22 15:06:35 +0200

    naw 19849: Product and Company changed Price & LinkedList

--- Company.java
+++ Company.java
@@ -1,4 +1,4 @@
 public class Company {
-	private ArrayList<Product> products;
+	private LinkedList<Product> products;
 	private String name = "IBM";
 }
--- Product.java
+++ Product.java
@@ -1,3 +1,4 @@
 public class Produkt{
 	private String name "Coca Cola";
+	private float price = 10;
 }
